9a69ac010df1b08b5286308df14e76d69e1b2165
mooziii <63669478+mooziii@users.noreply.github.com>
2022-04-21 16:23:47 +0200

    Make event priority work

--- src/main/java/me/obsilabor/alert/EventManager.java
+++ src/main/java/me/obsilabor/alert/EventManager.java
@@ -3,16 +3,15 @@ package me.obsilabor.alert;
 import me.obsilabor.alert.kotlin.KotlinListener;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Manages all events
  */
 public class EventManager {
 
-    private static final List<Object> listeners = new ArrayList<>();
+    private static final HashMap<Object, List<Method>> listeners = new HashMap<>();
 
     /**
      * Calls an event and triggers all listeners (without showing exceptions)
@@ -30,7 +29,7 @@ public class EventManager {
      * @param <T> Type of the event
      */
     public static  <T extends Event> void callEvent(T event, boolean printException) {
-        for (Object listener : listeners) {
+        for (Object listener : listeners.keySet()) {
             try {
                 Method method = listener.getClass().getMethod("isActive");
                 boolean isActive = (boolean) method.invoke(listener);
@@ -38,11 +37,7 @@ public class EventManager {
                     continue;
                 }
             } catch (Exception ignored) {}
-            for (Method method : listener.getClass().getDeclaredMethods()) {
-                Subscribe subscribe = method.getAnnotation(Subscribe.class);
-                if(subscribe == null) {
-                    continue;
-                }
+            for (Method method : listeners.get(listener)) {
                 if(method.getParameterCount() != 1) {
                     System.out.println("[ALERT]: @Subscribe method with more or less then 1 arguments can't be invoked: " + method.getName());
                     continue;
@@ -66,7 +61,21 @@ public class EventManager {
      * @param listener the listener to register
      */
     public static void registerListener(Object listener) {
-        listeners.add(listener);
+        HashMap<Method, Integer> priorityMap = new HashMap<>();
+        for (Method method : listener.getClass().getDeclaredMethods()) {
+            Subscribe subscribe = method.getAnnotation(Subscribe.class);
+            if(subscribe == null) {
+                continue;
+            }
+            priorityMap.put(method, subscribe.priority());
+        }
+        List<Method> list = priorityMap.entrySet().stream().sorted(Map.Entry.comparingByValue()).map(Map.Entry::getKey).collect(Collectors.toList());
+        Collections.reverse(list);
+        listeners.put(listener, list);
+        System.out.println(listeners);
+    }
+
+    /**
     }
 
 }
